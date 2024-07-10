25745a831cb9749cb2e63b580f5975a10548f873
AsifRiseofPower <aloasif31@gmail.com>
2024-07-10 05:48:38 +0530

    Logo Change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import netfliclogo from '../assets/logo.svg'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
      <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
          <div className='text-red-600 cursor-pointer font-bold text-4xl'>
-            <img src={netfliclogo} className='h-10' alt="Netflix" />
+            <h1>REACTFLIX</h1>
           </div>
         <div>
         <select name="" id="" className='bg-gray-950 border-[1px] border-gray-300 text-white md:h-10 md:w-24'>
